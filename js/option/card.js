5401fc02491cc34ea61b3655b2e0b89937e65797
chigayuki <107536212+chikinaN@users.noreply.github.com>
2023-10-20 12:18:08 +0900

    Update card.js

--- js/option/card.js
+++ js/option/card.js
@@ -8,44 +8,44 @@ const formatUnderLine = () => {
   const cardElements = document.getElementsByClassName('card');
   for (let i = 0; i < cardElements.length; i++) {
     const card = cardElements[i];
-    const cardText = card.querySelector('aside > p');
-    const fontSize = parseInt(window.getComputedStyle(cardText).getPropertyValue('font-size'));
-    const textLength = Math.floor(cardText.offsetWidth / fontSize);
-    function formatText(initializeText) {
-      const cleanedText = initializeText.replace(/<\/?span[^>]*>/g, '');
-      return cleanedText;
-    }
-
-    const formatCardText = (inputString, numChars) => {
-      const resultArray = [];
-      let currentChunk = '';
-      let charCount = 0;
-      for (let i = 0; i < inputString.length; i++) {
-        if (inputString[i] === '<' && inputString[i + 1] === 'b' && inputString[i + 2] === 'r' && inputString[i + 3] === '>') {
-          if (currentChunk.length > 0) {
-            resultArray.push(currentChunk);
-            currentChunk = '';
-            charCount = 0;
+    const cardTexts = card.querySelectorAll('aside > p')
+    Array.from(cardTexts).forEach(cardText => {
+      const fontSize = parseInt(window.getComputedStyle(cardText).getPropertyValue('font-size'));
+      const textLength = Math.floor(cardText.offsetWidth / fontSize);
+      function formatText(initializeText) {
+        const cleanedText = initializeText.replace(/<\/?span[^>]*>/g, '');
+        return cleanedText;
+      }
+      const formatCardText = (inputString, numChars) => {
+        const resultArray = [];
+        let currentChunk = '';
+        let charCount = 0;
+        for (let i = 0; i < inputString.length; i++) {
+          if (inputString[i] === '<' && inputString[i + 1] === 'b' && inputString[i + 2] === 'r' && inputString[i + 3] === '>') {
+            if (currentChunk.length > 0) {
+              resultArray.push(currentChunk);
+              currentChunk = '';
+              charCount = 0;
+            }
+            i += 3;
+          } else {
+            if ((charCount === numChars) || (charCount + 1 === numChars && inputString[i + 1] === '。')) {
+              resultArray.push(currentChunk);
+              currentChunk = '';
+              charCount = 0;
+            }
+            currentChunk += inputString[i];
+            charCount++;
           }
-          i += 3;
-        } else {
-          if ((charCount === numChars) || (charCount + 1 === numChars && inputString[i + 1] === '。')) {
-            resultArray.push(currentChunk);
-            currentChunk = '';
-            charCount = 0;
-          }
-          currentChunk += inputString[i];
-          charCount++;
         }
-      }
-      if (currentChunk.length > 0) {
-        resultArray.push(currentChunk);
-      }
-      return resultArray;
-    };
-
-    const formattedArray = formatCardText(formatText(initializeElements[i]), textLength);
-    cardText.innerHTML = formattedArray.map(item => `<span>${item}</span>`).join(' ');
+        if (currentChunk.length > 0) {
+          resultArray.push(currentChunk);
+        }
+        return resultArray;
+      };
+      const formattedArray = formatCardText(formatText(initializeElements[i]), textLength);
+      cardText.innerHTML = formattedArray.map(item => `<span>${item}</span>`).join(' ');
+    })
   }
 }
 
