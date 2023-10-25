d90058e2fb80a012d79838766c52960bc99a39a3
yuuta-0509 <140583267+yuuta-0509@users.noreply.github.com>
2023-10-25 21:58:50 +0900

    最後の微調整(food)

--- js/food.js
+++ js/food.js
@@ -1,10 +1,12 @@
 function openTab(tabNumber) {
-    var i, tabContents;
-
-    tabContents = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContents.length; i++) {
+    const tabContents = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabContents.length; i++) {
         tabContents[i].classList.remove("active");
     }
+    Array.from(document.getElementsByClassName('tab')).forEach(tab => {
+        tab.classList.remove('tab-active')
+    })
 
     document.getElementById("tab" + tabNumber).classList.add("active");
+    event.target.classList.add('tab-active')
 }
